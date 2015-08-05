c3b910fa8c313270767d1bd890641b90e2747d42
Pim Derks <info@pimderks.com>
2015-08-05 13:11:05 +0200

    Output multiple AMD packages.

--- gulp/gulp.amd.js
+++ gulp/gulp.amd.js
@@ -4,18 +4,30 @@ var gulp = require('gulp'),
 
 module.exports = function(){
 
-    var dir = config.roots.www + '/' + config.paths.static + '/' + config.paths.ui,
+    var dir = config.roots.src + '/' + config.paths.static + '/' + config.paths.ui,
         files = dir + '/**/*',
-        dest = dir;
+        dest = config.roots.www + '/' + config.paths.static + '/' + config.paths.ui;
 
-    return gulp.src(files)
-        .pipe(amdOptimize("main", {
-            "paths": {
-                "conditioner": 'vendor/conditioner/conditioner',
-                "modernizr": 'vendor/modernizr/modernizr'
-            }
-        }))
-        .pipe(concat("main-build.js"))
-        .pipe(gulp.dest(dest));
+    // Modules to compile. We must use the actual file path here.
+    var modules = ["main", "07-modules/Module/js/Module"];
+
+    modules.forEach(function(module){
+        gulp.src(files)
+            .pipe(amdOptimize(module, {
+                "paths": {
+                    "conditioner": 'vendor/conditioner/conditioner',
+                    "modernizr": 'vendor/modernizr/modernizr',
+                    'utils': '01-utils',
+                    'base': '03-base',
+                    'generic': '04-generic',
+                    'objects': '05-objects',
+                    'components': '06-components',
+                    'modules': '07-modules'
+                },
+                "baseUrl": dir
+            }))
+            .pipe(concat(module + '.js'))
+            .pipe(gulp.dest(dest));
+    });
 
 };
