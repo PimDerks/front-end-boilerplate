c01af93a3bf4c1622a1f96e4fcb46fca8d5b459d
Pim Derks <info@pimderks.com>
2015-08-22 02:11:44 +0200

    Added CSS selector count script.

--- src/static/js/debug.js
+++ src/static/js/debug.js
@@ -2,7 +2,10 @@ var debug = function(){
 
     'use strict';
 
-    // baseline
+    /*
+     * Insert toggle for baseline.
+     */
+
     var toggle = document.createElement('a');
     toggle.href = '#';
     toggle.setAttribute('style', 'position: fixed; bottom: 0; right: 0; color: white; padding: .75em; background: black; z-index: 10000;');
@@ -13,7 +16,52 @@ var debug = function(){
     document.body.appendChild(toggle);
     document.body.classList.add('baseline');
 
-    // append debug stylesheet
+    /*
+     * Count stylesheet lines/selectors.
+     */
+
+    function countCSSRules() {
+        var results = '',
+            log = '';
+        if (!document.styleSheets) {
+            return;
+        }
+        for (var i = 0; i < document.styleSheets.length; i++) {
+            countSheet(document.styleSheets[i]);
+        }
+        function countSheet(sheet) {
+            var count = 0, countRules = 0;
+            if (sheet && sheet.cssRules) {
+                for (var j = 0, l = sheet.cssRules.length; j < l; j++) {
+                    if (!sheet.cssRules[j].selectorText) {
+                        continue;
+                    }
+                    count += sheet.cssRules[j].selectorText.split(',').length;
+
+                }
+                countRules += sheet.cssRules.length;
+
+                log += '\nFile: ' + (sheet.href ? sheet.href : 'inline <style> tag');
+                log += '\nRules: ' + sheet.cssRules.length;
+                log += '\nSelectors: ' + count;
+                log += '\n--------------------------';
+                if (count >= 4096) {
+                    results += '\n********************************\nWARNING:\n There are ' + count + ' CSS Selectors in the stylesheet ' + sheet.href + ' - IE will ignore the last ' + (count - 4096) + ' selectors!\n';
+                } else if (countRules >= 4096) {
+                    results += '\n********************************\nWARNING:\n There are ' + countRules + ' CSS rules in the stylesheet ' + sheet.href + ' - IE will ignore the last ' + (countRules - 4096) + ' rules!\n';
+                }
+            }
+        }
+        console.log(log);
+        console.log(results);
+    };
+
+    countCSSRules();
+
+    /*
+     * Append debug stylesheet
+     */
+
     loadCSS('/static/css/debug.css');
 
 };
