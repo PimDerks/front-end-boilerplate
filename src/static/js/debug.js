2052ba8704cd2091304675e7e17953ea241e686b
Pim Derks <info@pimderks.com>
2015-08-22 02:24:07 +0200

    Made debugger configurable and added A11Y. #48

--- src/static/js/debug.js
+++ src/static/js/debug.js
@@ -1,4 +1,4 @@
-var debug = function(){
+var debug = function(config){
 
     'use strict';
 
@@ -6,66 +6,79 @@ var debug = function(){
      * Insert toggle for baseline.
      */
 
-    var toggle = document.createElement('a');
-    toggle.href = '#';
-    toggle.setAttribute('style', 'position: fixed; bottom: 0; right: 0; color: white; padding: .75em; background: black; z-index: 10000;');
-    toggle.innerHTML = 'Toggle Baseline';
-    toggle.addEventListener('click', function(){
-        document.body.classList.toggle('baseline');
-    });
-    document.body.appendChild(toggle);
-    document.body.classList.add('baseline');
+    if(config.BASELINE) {
+        var toggle = document.createElement('button');
+        toggle.type = 'button';
+        toggle.setAttribute('style', 'position: fixed; bottom: 0; right: 0; color: white; padding: .75em; background: black; z-index: 10000;');
+        toggle.innerHTML = 'Toggle Baseline';
+        toggle.addEventListener('click', function () {
+            document.body.classList.toggle('baseline');
+        });
+        document.body.appendChild(toggle);
+        document.body.classList.add('baseline');
+    }
 
     /*
      * Count stylesheet lines/selectors.
      */
 
-    function countCSSRules() {
-        var results = '',
-            log = '';
-        if (!document.styleSheets) {
-            return;
-        }
-        for (var i = 0; i < document.styleSheets.length; i++) {
-            countSheet(document.styleSheets[i]);
-        }
-        function countSheet(sheet) {
-            var count = 0, countRules = 0;
-            if (sheet && sheet.cssRules) {
-                for (var j = 0, l = sheet.cssRules.length; j < l; j++) {
-                    if (!sheet.cssRules[j].selectorText) {
-                        continue;
+    if(config.COUNT) {
+        (function countCSSRules() {
+            var results = '',
+                log = '';
+            if (!document.styleSheets) {
+                return;
+            }
+            for (var i = 0; i < document.styleSheets.length; i++) {
+                countSheet(document.styleSheets[i]);
+            }
+            function countSheet(sheet) {
+                var count = 0, countRules = 0;
+                if (sheet && sheet.cssRules) {
+                    for (var j = 0, l = sheet.cssRules.length; j < l; j++) {
+                        if (!sheet.cssRules[j].selectorText) {
+                            continue;
+                        }
+                        count += sheet.cssRules[j].selectorText.split(',').length;
+
                     }
-                    count += sheet.cssRules[j].selectorText.split(',').length;
+                    countRules += sheet.cssRules.length;
 
-                }
-                countRules += sheet.cssRules.length;
-
-                log += '\nFile: ' + (sheet.href ? sheet.href : 'inline <style> tag');
-                log += '\nRules: ' + sheet.cssRules.length;
-                log += '\nSelectors: ' + count;
-                log += '\n--------------------------';
-                if (count >= 4096) {
-                    results += '\n********************************\nWARNING:\n There are ' + count + ' CSS Selectors in the stylesheet ' + sheet.href + ' - IE will ignore the last ' + (count - 4096) + ' selectors!\n';
-                } else if (countRules >= 4096) {
-                    results += '\n********************************\nWARNING:\n There are ' + countRules + ' CSS rules in the stylesheet ' + sheet.href + ' - IE will ignore the last ' + (countRules - 4096) + ' rules!\n';
+                    log += '\nFile: ' + (sheet.href ? sheet.href : 'inline <style> tag');
+                    log += '\nRules: ' + sheet.cssRules.length;
+                    log += '\nSelectors: ' + count;
+                    log += '\n--------------------------';
+                    if (count >= 4096) {
+                        results += '\n********************************\nWARNING:\n There are ' + count + ' CSS Selectors in the stylesheet ' + sheet.href + ' - IE will ignore the last ' + (count - 4096) + ' selectors!\n';
+                    } else if (countRules >= 4096) {
+                        results += '\n********************************\nWARNING:\n There are ' + countRules + ' CSS rules in the stylesheet ' + sheet.href + ' - IE will ignore the last ' + (countRules - 4096) + ' rules!\n';
+                    }
                 }
             }
-        }
-        console.log(log);
-        console.log(results);
-    };
 
-    countCSSRules();
+            console.log(log);
+            console.log(results);
+        }());
+    }
+
+    /*
+     * Append A11Y debug stylesheet
+     */
+
+    if(config.A11Y){
+        loadCSS('https://rawgit.com/ffoodd/a11y.css/master/css/a11y-en.css');
+    }
 
     /*
      * Append debug stylesheet
      */
 
-    loadCSS('/static/css/debug.css');
+    if(config.CSS) {
+        loadCSS('/static/css/debug.css');
+    }
 
 };
 
 if(window.DEBUG){
-    debug();
+    debug(window.DEBUG);
 }
